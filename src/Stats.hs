1652fcb756070352f1ae6983928335064e02abe7
Nazarii Brenchuk <bnazariy@gmail.com>
2016-05-29 21:18:58 +0300

    Added stat properties.

--- src/Stats.hs
+++ src/Stats.hs
@@ -5,32 +5,43 @@ import Text.Printf
 import Data.List(sort)
 
 type Selection = [[Float]]
+type Interval = (Float, Float)
+type Histogram = [(Interval, [Float])]
+type IntervalFunc = Selection -> Float
+
+data HistogramOpt a = HistogramOpt {
+                        calcFunc :: (Histogram -> a)
+                       ,name :: String
+                    }
 
 size :: Selection -> Int
 size s = sum (map length s)
 
-n :: Selection -> Float
+hSize :: Histogram -> Int
+hSize h = sum (map (length.snd) h)
+
+n :: IntervalFunc
 n [] = 0
 n s = 1 + 3.332 * logBase 10 ( fromIntegral $ size s )
 
-nDjun :: Selection -> Float
+nDjun :: IntervalFunc
 nDjun [] = 0
 nDjun s = 0.51 * sqrt (fromIntegral $ size s )
 
-intervalWidth :: (Selection -> Float) -> Selection -> Float
+intervalWidth :: IntervalFunc -> Selection -> Float
 intervalWidth _ [] = 0
 intervalWidth f s = round' 2 ((max / min) / f s)
     where
         max = maximum (map maximum s)
         min = minimum  (map minimum s)
 
-allInRange :: (Selection -> Float) -> Float -> Selection -> [Float]
+allInRange :: IntervalFunc -> Float -> Selection -> [Float]
 allInRange f start a =
     concat [ [x | x <- y, x >= start && x < (start + iw)  ] | y <- a ]
     where
         iw = intervalWidth f a
 
-groupSelection :: (Selection -> Float) -> Selection -> [((Float, Float), [Float])]
+groupSelection :: IntervalFunc  -> Selection -> Histogram
 groupSelection _ [] = []
 groupSelection f a = [ tupleInRange $ round' 2 x | x <- [0,iw..max'] ]
     where
@@ -38,9 +49,38 @@ groupSelection f a = [ tupleInRange $ round' 2 x | x <- [0,iw..max'] ]
         width = length (head a) -- selection is rectangular.
         iw = intervalWidth f a
         max' = maximum (map maximum a)
+createHistogram = groupSelection
+
+ints :: Histogram -> [(Interval, Int)]
+ints g = map (\(i, l) -> (i, length l)) g
+
+intervals :: HistogramOpt [(Interval, Int)]
+intervals = HistogramOpt  {calcFunc = ints, name = "Intervals"}
+
+middleIntervals :: HistogramOpt [(Interval, Float)]
+middleIntervals = HistogramOpt { calcFunc = map (\((a, b), _) -> ((a, b), (a + b) / 2) ), name = "Middles" }
+
+-- Ni/N
+dentities :: HistogramOpt [(Interval, Float)]
+dentities = HistogramOpt { calcFunc = (\hist -> map (\(i, l) -> (i, fromIntegral l / (fromIntegral $ hSize hist)) ) (ints hist) ), name = "Densts"  }
+
+si :: HistogramOpt [(Interval, Int)]
+si = HistogramOpt { calcFunc = (\hist -> map (\(n, (i, _)) -> (i, summ hist n) ) (idx hist) ), name = "Si" }
+    where
+        idx g = zip [1..] (ints g)
+        summ g n = sum [ snd ( (ints g)!!k) | k <- [0..(n-1)] ]
+
+fi :: HistogramOpt [(Interval, Float)]
+fi = HistogramOpt { calcFunc = (\hist -> map (\(n, (i, _)) -> (i, summ hist n) )  (idx hist) ), name = "Fi" }
+    where
+        idx g = zip [1..] (ints g)
+        summ :: Histogram -> Int -> Float
+        summ g n = sum [ (fromIntegral (snd ((ints g)!!k))) / ( fromIntegral $ hSize g) | k <- [0..(n-1)] ]
+applyOption :: Histogram -> HistogramOpt a -> (a, String)
+applyOption g opt = ( (calcFunc opt) g, name opt)
 
 realData :: Selection
-realData = [
+realData = map (map (round' 2)) [
   [x - 0.2 | x <- [2.9,4.1,4.2,5.2,5.3,5.4, 5.0, 7.0, 7.9, 8.4]]
  ,[3.1, 4.3, 4.4, 5.4, 5.5, 5.6, 6.2, 7.2, 8.1, 8.6]
  ,[9.9, 5.2, 6.9, 8.9, 9.1, 3.3, 4.5, 8.1, 9.5, 10.5]
@@ -116,13 +156,15 @@ pics f selection = [
         cl = smallClosest step
         render = (`renderTuple` step)
 
---round' :: Int -> Float -> Float
+round' :: Int -> Float -> Float
 round' n fl = fromInteger (round (fl * (10 ^ n))) / (10 ^ n)
 
+points = pictures  [line [(x, (1/50) * ((x)^2))| x <- [-100,0.0001..100]]]
+
 flip' (a, b) = (b, a)
-viewPort = ViewPort (-50, -100) (90*0) 3
+viewPort = ViewPort (-50, -100) (90*0) 6
 
 runW = do
     let window = InWindow "W" (900, 800) (100, 100)
-    display window  white ( applyViewPortToPicture viewPort $ pictures $ pics n realData )
+    display window  white ( applyViewPortToPicture viewPort $ pictures $ pics n realData ) 
 
